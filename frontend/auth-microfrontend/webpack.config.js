6ceecc1fc0e68204b969306b153b6edec3cde177
Pavel Lobanov <pslobanov@avito.ru>
2024-08-04 17:17:44 +0300

    Fix namings and close button

--- frontend/auth-microfrontend/webpack.config.js
+++ frontend/auth-microfrontend/webpack.config.js
@@ -32,9 +32,23 @@ module.exports = {
                 use: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/,
-                use: ['file-loader']
+                test: /\.svg$/,
+                type: "asset/inline",
+                // Inline assets with the "inline" query parameter.
+                resourceQuery: /inline/,
             },
+            {oneOf: [
+                {
+                    test: /\.svg$/,
+                    type: "asset/inline",
+                    // ...
+                },
+                {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: 'asset/resource'
+                    // ...
+                },
+            ]},
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: ['file-loader']
